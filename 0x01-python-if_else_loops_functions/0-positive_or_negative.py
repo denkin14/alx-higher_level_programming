54ecb252ce5096ddf1d42f5b1caa684641d039a5
panda <dennismuguongo@gmail.com>
2022-06-28 18:59:16 +0300

    positive and negative

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+import random
+from this import d
+from turtle import clear
+number = random.randint(-10, 10)
+if number > 0:
+    print(f"{number:d} is positive")
+elif number == 0:
+    print(f"{number:d} is zero")
+else:
+    print(f"{number:d} is negative")
