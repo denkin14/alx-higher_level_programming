fae649300aa2283c08d81135047a28c3a1935021
denkin14 <dennismuguongo@gmail.com>
2022-06-28 17:02:20 +0000

    fixed task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 
 for my_digit1 in range (0, 10):
     for my_digit2 in range(my_digit1 + 1, 10):
-        if my_digit2 == 8 and my_digit2 == 9:
+        if my_digit1 == 8 and my_digit1 == 9:
             print("{}{}".format(my_digit1, my_digit2))
         else:
             print("{}{}".format(my_digit1, my_digit2), end=", ")
