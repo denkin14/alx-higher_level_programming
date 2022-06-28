99c5b23823500d80f043c23f032e49c995097353
denkin14 <dennismuguongo@gmail.com>
2022-06-28 17:41:49 +0000

    pycodestyle fix

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-"""Print the numbers from 1 to 100 separated by a space.
-  multiples of three, print Fizz instead of the number
-  multiples of five, print Buzz instead of the number.
-  multiples of three and five, print FizzBuzz instead of the number.
-  """
 
 def fizzbuzz():
     for number in range(1, 101):
