6597f887513d8dc570815375c532ecc638ca634d
denkin14 <dennismuguongo@gmail.com>
2022-06-28 17:12:25 +0000

    fixed comment

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-"""this function checks for lowercse characters"""
 def islower(c):
+    """This function checks for lowercse characters"""
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
