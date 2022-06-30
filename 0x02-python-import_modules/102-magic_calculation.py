e501897f2fe2ea6f8cb75cbe5d5bf0c8d8594597
denkin14 <dennismuguongo@gmail.com>
2022-06-30 15:28:34 +0000

    ByteCode -> python #3 fix

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    """"Bytecode to match to Holberton"""
+    """bytecod to matche to Holberton School."""
     from magic_calculation_102 import add, sub
 
     if a < b:
@@ -9,6 +9,6 @@ def magic_calculation(a, b):
         for i in range(4, 6):
             c = add(c, i)
         return (c)
-    
+
     else:
         return(sub(a, b))
