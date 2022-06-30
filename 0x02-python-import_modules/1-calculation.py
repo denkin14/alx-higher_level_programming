5c2f7a57ac536672a4de149818a48caa8364a45f
denkin14 <dennismuguongo@gmail.com>
2022-06-30 12:25:52 +0000

    fixed toolbox

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
-    """print sum, differenece & quotient of 10 and 5"""
+if __name__ == "__main__":
+    """Print sum, difference, multiple and quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
