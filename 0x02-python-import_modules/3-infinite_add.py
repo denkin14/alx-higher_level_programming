0098d96e3b36897e8d479742f2b33e5a5e72af93
denkin14 <dennismuguongo@gmail.com>
2022-06-30 12:51:52 +0000

    infinite numbers fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """Print addition of all arguments."""
     import sys
-    
+
     total = 0
-    for d in range(len(sys.argv) - 1):
-        total += int(sys.argv[d + 1])
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
     print("{}".format(total))
