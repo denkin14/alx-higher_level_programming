2b1b06af43ab08ef6dc71a0880467d15af1ee7f5
denkin14 <dennismuguongo@gmail.com>
2022-07-06 20:26:20 +0000

    roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for d in list_num:
+        if max_list > d:
+            to_sub += d
+
+    return (max_list - to_sub)
+
+
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+
+    if not isinstance(roman_string, str):
+        return 0
+
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
+
+    num = 0
+    last_rom = 0
+    list_num = [0]
+
+    for char in roman_string:
+        for r_num in list_keys:
+            if r_num == char:
+                if rom_n.get(char) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n.get(char)]
+                else:
+                    list_num.append(rom_n.get(char))
+
+                last_rom = rom_n.get(char)
+
+    num += to_subtract(list_num)
+
+    return (num)
