717445fd889fa6f70112adf7e9bb0dba0c4264e6
denkin14 <dennismuguongo@gmail.com>
2022-07-18 12:39:37 +0000

    minor fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x =0):
+def safe_print_list(my_list=[], x=0):
     """prints x elements of list
 
     my_list(list): the list to print elements from
